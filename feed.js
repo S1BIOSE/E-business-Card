9e2e5addf77338e362ddbff451f820e39a0ba681
S1BIOSE <81697984+S1BIOSE@users.noreply.github.com>
2021-04-08 10:08:11 +0200

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,22 +1,26 @@
-const FEED_URL = `https://www.s1biose.com/fr/test/rss.xml`;
+const FEED_URL = `https://www.s1biose.com/fr/testfeedrss/rss.xml`;
 
 fetch(FEED_URL)
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
-    console.log(data);
     const items = data.querySelectorAll("item");
     let html = ``;
     items.forEach(el => {
       html += `
-        <article>
-          <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
-          <h2>
-            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener noreferrer">
-              ${el.querySelector("title").innerHTML}
-            </a>
-          </h2>
-        </article>
+        <div class="card position-relative text-start text-dark mb-3 p-2">
+          <div class="d-flex align-items-center">
+            <div class="flex-shrink-0">
+              <img class="rounded overflow-hidden" src="${el.querySelector("enclosure").getAttribute('url')}" alt="${el.querySelector("title").innerHTML}">
+            </div>
+            <div class="flex-grow-1 ms-3">
+              <div class="h5">${el.querySelector("title").innerHTML}</div>
+              <p class="mb-2">${el.querySelector("description").innerHTML}</p>
+              <p class="mb-0"><small class="text-muted">${el.querySelector("pubDate").innerHTML}</small></p>
+            </div>
+          </div>
+          <a href="${el.querySelector("link").innerHTML}" title="${el.querySelector("title").innerHTML}" class="stretched-link" target="_blank" rel="noopener noreferrer"></a>
+        </div>
       `;
     });
     document.querySelector('#feed').insertAdjacentHTML("beforeend", html);
