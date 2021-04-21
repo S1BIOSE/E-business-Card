77c6db3e7416a3c52c53cbc6a53e02b99972a515
S1BIOSE <81697984+S1BIOSE@users.noreply.github.com>
2021-04-21 13:47:36 +0200

    Update btn-clipboard.js

--- btn-clipboard.js
+++ btn-clipboard.js
@@ -1,9 +1,3 @@
-var clipboard = new ClipboardJS('.btn-clipboard');
-
-clipboard.on('success', function (e) {
-console.log(e);
-});
-
-clipboard.on('error', function (e) {
-console.log(e);
+new ClipboardJS('.btn-clipboard', {
+  container: document.getElementById('settingModal')
 });
